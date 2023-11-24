4021008788f450dd864e7e7b9b51ffcd9caf025f
Max Davidse <m.davidse@rubicon.nl>
2023-11-24 12:55:18 +0100

    Made the run animation work when we are moving

--- Assets/_Project/Scripts/PlayerMovement.cs
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -5,14 +5,18 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float _runSpeed = 10f;
+    [SerializeField] float _runSpeed = 5f;
 
     Vector2 _moveInput;
     Rigidbody2D _rigidbody;
+    Animator _playerAnimator;
+
+    bool _playerHasHorizontalMoveSpeed = false;
 
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
     }
 
     void Update()
@@ -29,13 +33,16 @@ public class PlayerMovement : MonoBehaviour
     void Run()
     {
         Vector2 playerVelocity = new Vector2(_moveInput.x * _runSpeed, _rigidbody.velocity.y);
+        _playerHasHorizontalMoveSpeed = Mathf.Abs(playerVelocity.x) > Mathf.Epsilon;
         _rigidbody.velocity = playerVelocity;
+
+        _playerAnimator.SetBool("isRunning", _playerHasHorizontalMoveSpeed);
     }
 
     void FlipSprite()
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidbody.velocity.x) > Mathf.Epsilon;
-        if (playerHasHorizontalSpeed)
+        _playerHasHorizontalMoveSpeed = Mathf.Abs(_rigidbody.velocity.x) > Mathf.Epsilon;
+        if (_playerHasHorizontalMoveSpeed)
         {
             transform.localScale = new Vector2(Mathf.Sign(_rigidbody.velocity.x), 1f);
         }
